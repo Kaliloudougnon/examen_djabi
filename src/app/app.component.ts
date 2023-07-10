c17b1898b2de8328383fc82599b13cfbd9a0a2d3
kalilou dougnon <dougnonkalilou@gmail.com>
2023-07-10 23:32:57 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {EtudiantService} from "./etudiant.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'consult_exam_front';
+  etudiant: any;
+  forms: FormGroup;
+
+  ngOnInit(): void {
+
+
+  }
+
+  constructor(public service: EtudiantService, public fb: FormBuilder) {
+    this.forms = this.fb.group(
+      {
+        numPlace: this.fb.control(""),
+        session: this.fb.control("")
+      }
+    )
+  }
+
+  chercherResultat() {
+    this.service.getEtudiant(this.forms?.value.numPlace,this.forms?.value.session).subscribe(
+      (data) => {
+        this.etudiant = data;
+      }
+    )
+  }
 }
