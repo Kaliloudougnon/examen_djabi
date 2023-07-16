73fc6e95db45dcf58a7cd0434f0e0b5db1ff755a
kalilou dougnon <dougnonkalilou@gmail.com>
2023-07-16 02:29:34 +0100

    second commit3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,20 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 })
 export class AppComponent implements OnInit {
   title = 'consult_exam_front';
+  isFindData = false;
   etudiant: any;
   forms: FormGroup;
   allEtudiant: any;
+  allEtudiantTse: any = [];
+  allEtudiantTsexp: any = [];
+  allEtudiantTseco: any = [];
+  allEtudiantTss: any = [];
+  allEtudiantTll: any = [];
+  allEtudiantTal: any = [];
   pagination: number = 0;
-  spinner=false;
+  spinner = false;
+  serie: any;
+
   ngOnInit(): void {
 
 
@@ -31,28 +40,90 @@ export class AppComponent implements OnInit {
   }
 
   chercherResultat() {
-this.spinner=true;
+    this.spinner = true;
     if (this.forms?.value.numPlace != null) {
       this.service.getEtudiant(this.forms?.value.numPlace, this.forms?.value.session).subscribe(
         (data) => {
           this.etudiant = data;
-          this.allEtudiant=null;
-          this.spinner=false;
+          this.allEtudiant = null;
+          this.spinner = false;
         }
       )
     }
     if (this.forms?.value.numPlace == null) {
       this.service.getAllEtudiant(this.forms?.value.academie, this.forms?.value.session).subscribe(
         (data) => {
-          this.allEtudiant = data;
-          this.etudiant=null;
-          this.spinner=false;
-          console.log(data);
+          this.allEtudiantTse = [];
+          this.allEtudiantTsexp = [];
+          this.allEtudiantTseco = [];
+          this.allEtudiantTss = [];
+          this.allEtudiantTll = [];
+          this.allEtudiantTal = [];
+          this.isFindData = true;
+          this.etudiantBySerie(data);
+          this.etudiant = null;
+          this.spinner = false;
+
         }
       )
     }
   }
+
   renderPage(event: number) {
     this.pagination = event;
+  }
+
+  etudiantBySerie(data: any) {
+    for (let i = 0; i < data.length; i++) {
+      switch (data[i].serie) {
+        case "TSE":
+          this.allEtudiantTse.push(data[i]);
+          break;
+        case "TSEXP":
+          this.allEtudiantTsexp.push(data[i]);
+          break;
+        case "TSECO":
+          this.allEtudiantTseco.push(data[i])
+          break;
+        case "TSS":
+          this.allEtudiantTss.push(data[i])
+          break;
+        case "TLL":
+          this.allEtudiantTll.push(data[i])
+          break;
+        case "TAL":
+          this.allEtudiantTal.push(data[i])
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  affichageBySerie(serie: string) {
+    this.serie = serie;
+    switch (serie) {
+      case "TSE":
+        this.allEtudiant = this.allEtudiantTse;
+        console.log(this.allEtudiant);
+        break;
+      case "TSEXP":
+        this.allEtudiant = this.allEtudiantTsexp;
+        break;
+      case "TSECO":
+        this.allEtudiant = this.allEtudiantTseco;
+        break;
+      case "TSS":
+        this.allEtudiant = this.allEtudiantTss;
+        break;
+      case "TLL":
+        this.allEtudiant = this.allEtudiantTll;
+        break;
+      case "TAL":
+        this.allEtudiant = this.allEtudiantTal;
+        break;
+      default:
+        break;
+    }
   }
 }
